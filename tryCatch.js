1d077064b3f8607c5b7a0c579b470bd5f1f459dd
Tae Lee <taebumlee@gmail.com>
2020-09-07 11:30:54 -0700

    Update tryCatch.js

--- tryCatch.js
+++ tryCatch.js
@@ -35,3 +35,84 @@ try {
   console.log(err.message);
   console.log(err.stack);
 }
+
+let json = '{"name":"John", "age":30}';
+let user = JSON.parse(json);
+
+console.log(user.name, user.age);
+
+let jsonTwo = "{bad json}";
+
+try {
+  let user = JSON.parse(jsonTwo);
+  console.log(user.name);
+} catch (e) {
+  console.log("데이터에 에러가 있어서 재요청을 시도",e.name, e.message);
+}
+
+let jsonThree = '{"age":30}';
+try {
+  let user = JSON.parse(jsonThree);
+  console.log(user.name); //name이 없으므로 undefined를 출력한다. 
+} catch (e) {
+  console.log("실행되지 않습니다.") // 입력에 문제가 없으므로 이 라인이 실행되지는 않는다. 
+}
+
+try {
+  JSON.parse("{잘못된 형식의 json o_O}");
+} catch(e) {
+  console.log(e.name, e.message);
+}
+
+let jsonFour = '{"age":30}'; //name key가 존재하지 않는 json객체
+try {
+  let user = JSON.parse(jsonFour);
+  if(!jsonFour.name) {
+    throw new SyntaxError("불완전한 데이터:이름 없음");
+  }
+  console.log(user.name); 
+} catch (e) {
+  console.log("JSON Error: " + e.message); // JSON Error: 불완전한 데이터 이름 없음
+}
+
+
+let jsonFive = '{ "age": 30 }'; // 불완전한 데이터
+try {
+  myUser = JSON.parse(jsonFive); // <-- user 앞에 let을 붙이는 걸 잊었네요.
+} catch(err) {
+  alert("JSON Error: " + err); // JSON Error: ReferenceError: user is not defined
+  // (실제론 JSON Error가 아닙니다.)
+}
+
+let jsonSix = '{"age":30}';
+try {
+  let user = JSON.parse(jsonSix);
+  if(!user.name) {
+    throw new SyntaxError("불완전한 데이터: 이름이 없음!")
+  }
+  blabla();
+  console.log(user.name);
+} catch(e) {
+  if(e instanceof SyntaxError) {
+    console.log("JSON Error: " + e.message);
+  } else {
+    throw e;
+  }
+}
+
+function readData() {
+  let json = '{"age":30}';
+  try {
+    blabla(); //error will be happened.
+  } catch (e) {
+    if(!(e instanceof SyntaxError)) {
+      throw e;
+    }
+  }
+}
+
+try {
+  readData(); //
+} catch(e) {
+  console.log("External catch got: " + e); 
+}
